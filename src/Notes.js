fdd8e73215c87c82b8d8d4df68fe30cbfe1473be
Jake Hertz <hertzjake@gmail.com>
2019-12-05 13:12:55 -0500

    error handling and clean up

--- src/Notes.js
+++ src/Notes.js
@@ -1,23 +1,7 @@
 /* eslint-disable react/button-has-type */
 import React, { Component } from 'react';
-import axios from 'axios';
 import { connect } from 'react-redux';
 import { createNote, deleteNote } from './index.js';
-
-
-// eslint-disable-next-line react/require-render-return
-// const Notes = props => {
-//         return (
-//             <div>
-//                 <ul>
-//                     {
-//                         props.notes.map(note => <li>{ note.text }</li>)
-//                     }
-//                 </ul>
-//                 <input type="text" name="newNote" /> <button>Create Note</button>
-//             </div>
-//         )
-// }
 
 class Notes extends Component {
     constructor(props) {
@@ -26,11 +10,6 @@ class Notes extends Component {
             invalidSubmit: false,
         }
     }
-    // handleChange() {
-    //     let newText = document.querySelector('input').value;
-    //     const { text } = this.state;
-    //     console.log(text)
-    // }
     handleSubmit(props) {
         if (props.notes.length === 5) {
             this.setState({invalidSubmit: true});
